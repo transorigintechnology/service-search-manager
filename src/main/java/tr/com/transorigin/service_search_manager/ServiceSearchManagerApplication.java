9d79cfdacc76c42604e9f97561cd6715251c7906
transorigintechnology <transorigintechnology@gmail.com>
2025-12-09 04:04:43 +0300

    project init

---
+++ src/main/java/tr/com/transorigin/service_search_manager/ServiceSearchManagerApplication.java
@@ -0,0 +1,13 @@
+package tr.com.transorigin.service_search_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSearchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSearchManagerApplication.class, args);
+	}
+
+}
